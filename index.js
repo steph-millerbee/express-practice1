252069ad64efe91e768dfea91e941967f4d28a76
Stephanie Miller <millerbee.steph@gmail.com>
2022-12-08 09:26:37 -0700

    assignment 1, create express server

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+// app.use(express.static("public"));
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+})
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000")
+}); 
